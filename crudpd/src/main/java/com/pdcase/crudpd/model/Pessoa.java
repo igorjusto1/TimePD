826dee9e25dea7bdadb96d4e00801b0285a36284
Igor Silva <igorsilvaspecial@gmail.com>
2020-10-19 15:13:30 -0300

    adicionado atributos da classe Pessoa

--- crudpd/src/main/java/com/pdcase/crudpd/model/Pessoa.java
+++ crudpd/src/main/java/com/pdcase/crudpd/model/Pessoa.java
@@ -1,8 +1,22 @@
 package com.pdcase.crudpd.model;
 
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table(name = "pessoa_teste")
 public class Pessoa {
-
+	  @Id
+	    @GeneratedValue
+	    private Long id;
+	  
+	  private String nome;
+	  
+	  private String sobrenome;
+	  
+	  private String cpf;
+	  
+	  private Date nascimento;
 }
