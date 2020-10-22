ffa2cb21181e1abc2c966030fa60839c732ec961
Igor Silva <igorsilvaspecial@gmail.com>
2020-10-22 17:56:59 -0300

    Fix: na hora da atualização da página de pessoas que não atualizava a lista. Feat: importação em batches de pessoas

--- crudpd/src/main/java/com/pdcase/crudpd/controller/PessoaController.java
+++ crudpd/src/main/java/com/pdcase/crudpd/controller/PessoaController.java
@@ -1,12 +1,25 @@
 package com.pdcase.crudpd.controller;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.model.file.UploadedFile;
 
 import com.pdcase.crudpd.model.Pessoa;
 import com.pdcase.crudpd.service.PessoaService;
@@ -30,8 +43,18 @@ public class PessoaController implements Serializable {
 	private transient Logger log;
 
 	// Modelo utilizado durante os requests
-	@Inject
 	private Pessoa newPessoa;
+
+	// Lista de pessoas na view
+	private List<Pessoa> pessoas;
+
+	// File para upload csv
+	private UploadedFile fileUpload;
+
+	@Inject
+	private FacesContext facesContext;
+
+
 
 	public Pessoa getNewPessoa() {
 		return newPessoa;
@@ -41,10 +64,26 @@ public class PessoaController implements Serializable {
 		this.newPessoa = newPessoa;
 	}
 
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
 
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
 
-	public List<Pessoa> getAllPessoas() {
-		return pessoaService.getAllPessoas();
+	public UploadedFile getFile() {
+		return fileUpload;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.fileUpload = file;
+	}
+
+	@PostConstruct
+	public void init() {
+		pessoas = pessoaService.getAllPessoas();
+		newPessoa = new Pessoa();
 	}
 
 	// Salva o objeto salvo no request
@@ -52,10 +91,17 @@ public class PessoaController implements Serializable {
 		try {
 			pessoaService.register(newPessoa);
 
-			newPessoa = new Pessoa();
-		
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registrado!",
+					"Registro de " + newPessoa.getNome() + " completado com sucesso");
+			facesContext.addMessage(null, m);
+
+			init();
+
 		} catch (Exception e) {
-			log.info(e.getMessage());
+			String errorMessage = getRootErrorMessage(e);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful");
+			facesContext.addMessage(null, m);
+			log.info(errorMessage);
 		}
 	}
 
@@ -63,10 +109,13 @@ public class PessoaController implements Serializable {
 	public void delete(int id) {
 		try {
 			pessoaService.delete(id);
-			newPessoa = new Pessoa();
+			init();
 		} catch (Exception e) {
+			String errorMessage = getRootErrorMessage(e);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Remoção não concluída");
+			facesContext.addMessage(null, m);
 
-			log.info(e.getMessage());
+			log.info(errorMessage);
 		}
 	}
 
@@ -74,9 +123,90 @@ public class PessoaController implements Serializable {
 	public void edit(int id) {
 		try {
 			newPessoa = pessoaService.edit(id);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição carregada!",
+					"Dados de " + newPessoa.getNome() + " carregados para edição com sucesso");
+			facesContext.addMessage(null, m);
 		} catch (Exception e) {
-			log.info(e.getMessage());
+			String errorMessage = getRootErrorMessage(e);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Edição não carregada");
+			facesContext.addMessage(null, m);
+
+			log.info(errorMessage);
 		}
 	}
 
+	public void upload() {
+		if (fileUpload != null) {
+
+			byte[] fileContent = fileUpload.getContent();
+
+			InputStream is = new ByteArrayInputStream(fileContent);
+			BufferedReader bfReader = new BufferedReader(new InputStreamReader(is));
+			String temp = null;
+
+			try {
+				int i = 1;
+				while ((temp = bfReader.readLine()) != null) {
+					String[] spli = temp.split(";");
+
+					String cpf = spli[2].replace("-", "");
+					cpf = cpf.replace(".", "");
+					newPessoa = new Pessoa();
+					newPessoa.setCpf(cpf);
+					newPessoa.setNome(spli[0]);
+					newPessoa.setSobrenome(spli[1]);
+					newPessoa.setNascimento(convertParaDateTime(i, spli[3]));
+					i++;
+
+					register();
+
+				}
+			} catch (IOException e) {
+				String errorMessage = getRootErrorMessage(e);
+				FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage,
+						"Erro no Upload de arquivo");
+				facesContext.addMessage(null, m);
+				log.info(errorMessage);
+			}
+			FacesMessage message = new FacesMessage("Successful", fileUpload.getFileName() + " carregado.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+
+	/**
+	 * @param i
+	 * @param spli
+	 */
+	private Date convertParaDateTime(int i, String dateValue) {
+
+		try {
+			return new SimpleDateFormat("dd/MM/yyyy").parse(dateValue);
+		} catch (ParseException e) {
+			String errorMessage = getRootErrorMessage(e);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage,
+					"Erro na conversão da linha " + i + " do arquivo");
+			facesContext.addMessage(null, m);
+			log.info(errorMessage);
+		}
+		return new Date();
+	}
+
+	private String getRootErrorMessage(Exception e) {
+		// Default to general error message that registration failed.
+		String errorMessage = "Operação falhou. Veja o log do servidor para mais informações";
+		if (e == null) {
+			// This shouldn't happen, but return the default messages
+			return errorMessage;
+		}
+
+		// Start with the exception and recurse to find the root cause
+		Throwable t = e;
+		while (t != null) {
+			// Get the message from the Throwable class instance
+			errorMessage = t.getLocalizedMessage();
+			t = t.getCause();
+		}
+		// This is the root cause message
+		return errorMessage;
+	}
 }
