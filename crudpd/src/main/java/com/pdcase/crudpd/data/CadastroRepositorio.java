e97a7f7746f1deedd165a48aca52d84837e72144
igorjusto1 <igor.justo@outlook.com>
2020-10-26 15:18:52 -0300

    Erros  e conflitos resolvidos

--- crudpd/src/main/java/com/pdcase/crudpd/data/CadastroRepositorio.java
+++ crudpd/src/main/java/com/pdcase/crudpd/data/CadastroRepositorio.java
@@ -13,16 +13,9 @@ import javax.transaction.UserTransaction;
 
 import com.pdcase.crudpd.model.Cadastro;
 
-<<<<<<< HEAD
-
- // Persistence Context serve pra definir qual configuração de acesso a banco de dados pra usar
- @PersistenceContext(name = "rcb_PU")
- public class CadastroRepositorio {
-=======
 // Persistence Context serve pra definir qual configuração de acesso a banco de dados pra usar
 @PersistenceContext(name = "rcb_PU")
 public class CadastroRepositorio {
->>>>>>> e6ee1909c9316b1e32a967bb29ccd98f9e9b6932
 	
 	// Gerenciador de acesso ao banco
 	@PersistenceContext
@@ -88,14 +81,7 @@ public class CadastroRepositorio {
 		CriteriaBuilder cb = em.getCriteriaBuilder();
 		CriteriaQuery<Cadastro> criteria = cb.createQuery(Cadastro.class);
 		Root<Cadastro> p = criteria.from(Cadastro.class);
-<<<<<<< HEAD
-		criteria.select(p).orderBy(cb.asc(p.get("nome")));
-		return em.createQuery(criteria).getResultList();
-	}
-}
-=======
 		criteria.select(p).orderBy(cb.asc(p.get("cidade")));
 		return em.createQuery(criteria).getResultList();
 	}
 }
->>>>>>> e6ee1909c9316b1e32a967bb29ccd98f9e9b6932
--- crudpd/src/main/java/com/pdcase/crudpd/model/Cadastro.java
+++ crudpd/src/main/java/com/pdcase/crudpd/model/Cadastro.java
@@ -1,13 +1,7 @@
 package com.pdcase.crudpd.model;
 
-<<<<<<< HEAD
-
 import java.io.Serializable;
 import java.util.List;
-=======
-import java.io.Serializable;
-//import java.util.List;
->>>>>>> e6ee1909c9316b1e32a967bb29ccd98f9e9b6932
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,81 +11,61 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-<<<<<<< HEAD
-
- @Entity
- @Table(name = "CADASTRO_TESTE")
-
- public class Cadastro  implements Serializable {
-		private static final long serialVersionUID = 1L;
- @Id
-=======
 @Entity
 @Table(name = "CADASTRO_TESTE")
 
 public class Cadastro implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	
 	@Id
->>>>>>> e6ee1909c9316b1e32a967bb29ccd98f9e9b6932
+
 	@SequenceGenerator(name = "CADASTRO_TESTE_SEQ", sequenceName = "CADASTRO_TESTE_SEQ", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CADASTRO_TESTE_SEQ")	
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CADASTRO_TESTE_SEQ")
 	@Column(name = "id")
 	private int id;
-<<<<<<< HEAD
- 
-     private String cidade;
-     private String bairro;
-     private int cep;
-=======
-	
+
 	private String cidade;
 	private String bairro;
 	private int cep;
->>>>>>> e6ee1909c9316b1e32a967bb29ccd98f9e9b6932
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+
 	public String getBairro() {
 		return bairro;
 	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+
 	public int getCep() {
 		return cep;
 	}
+
 	public void setCep(int cep) {
 		this.cep = cep;
 	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-<<<<<<< HEAD
+
 	public List<Cadastro> getAllCadastro() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-    
 
-  
- }
- 
-  
-=======
-  
- }
->>>>>>> e6ee1909c9316b1e32a967bb29ccd98f9e9b6932
+}
